efbfd1cb622f8c3c0d488030f4b21eb35515fc1e
Kinara0 <kinarasoni@yahoo.com>
2021-11-05 21:41:53 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// it can parse only integer but not any other data type together
+        // parse will take string and give premitive data type
         String number = "234";
+        int i = Integer.parseInt(number);
+        System.out.println(i);
+        double v = Double.parseDouble(number);
+        System.out.println(v);
+        Integer integer = Integer.valueOf(i);
+        System.out.println(i);
+        //parse return premitive type
+        //value of method return the relevant Number Object holding the value of the argument passed.
+
 
     }
 }
